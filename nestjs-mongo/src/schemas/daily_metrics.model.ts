3b4e018daa08b5546449686f275cfbcc87cfdb12
Mastered1738 <vinkomilotic632@gmail.com>
2024-01-27 15:50:40 +0100

    feat: idk anymore

--- nestjs-mongo/src/schemas/daily_metrics.model.ts
+++ nestjs-mongo/src/schemas/daily_metrics.model.ts
@@ -2,8 +2,18 @@ import mongoose from 'mongoose';
 
 export const DailyMetricSchema = new mongoose.Schema({
   _id: { type: mongoose.Types.ObjectId },
+  total_processed_olives: Number,
+  total_oil_made: Number,
+  analyzed_at_time: Date,
+  avarage_oil_percentage: Number,
 });
 
 export class DailyMetric {
-  constructor(public id: string) {}
+  constructor(
+    public id: string,
+    public total_processed_olives: number,
+    public total_oil_made: number,
+    public analyzed_at_time: Date,
+    public avarage_oil_percentage: number,
+  ) {}
 }
--- nestjs-mongo/src/schemas/receipts.model.ts
+++ nestjs-mongo/src/schemas/receipts.model.ts
@@ -2,8 +2,18 @@ import mongoose from 'mongoose';
 
 export const ReceiptSchema = new mongoose.Schema({
   _id: { type: mongoose.Types.ObjectId },
+  worker_name: String,
+  buyer_name: String,
+  amount: Number,
+  timestamp: Date,
 });
 
 export class Receipt {
-  constructor(public id: string) {}
+  constructor(
+    public id: string,
+    public worker_name: string,
+    public buyer_name: string,
+    public amount: number,
+    public timestamp: Date,
+  ) {}
 }
