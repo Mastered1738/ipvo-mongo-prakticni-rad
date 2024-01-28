00c43614b2e59d91a59645257284d8c544637cd5
Mastered1738 <vinkomilotic632@gmail.com>
2024-01-28 17:42:09 +0100

    feat: cron

--- nestjs-mongo/src/controllers/daily_metrics.controller.ts
+++ nestjs-mongo/src/controllers/daily_metrics.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+//import { Cron } from '@nestjs/schedule';
 import { DailyMetricService } from 'src/providers/daily_metrics.service';
 import { DailyMetric } from 'src/schemas/daily_metrics.model';
 
@@ -7,7 +7,6 @@ import { DailyMetric } from 'src/schemas/daily_metrics.model';
 export class DailyMetricController {
   constructor(private readonly dailyMetricService: DailyMetricService) {}
 
-  @Cron('0 0 22 * * *')
   @Post('/get-daily-metrics')
   async getDailyMetrics(): Promise<DailyMetric> {
     return this.dailyMetricService.getDailyMetrics();
--- nestjs-mongo/src/providers/daily_metrics.service.ts
+++ nestjs-mongo/src/providers/daily_metrics.service.ts
@@ -2,6 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { CACHE_MANAGER, CacheStore } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { Model } from 'mongoose';
 import { firstValueFrom } from 'rxjs';
 import { DailyMetric } from 'src/schemas/daily_metrics.model';
@@ -15,22 +16,26 @@ export class DailyMetricService {
     private readonly httpService: HttpService,
   ) {}
 
-  async getDailyMetrics(): Promise<DailyMetric> {
-    const response = await firstValueFrom(
-      this.httpService.post('http://nestjs-pos:3000/orders/daily_metrics'),
-    );
+  @Cron('* 41 17 * * *')
+  async getDailyMetrics(): Promise<any> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post('http://nestjs-pos:3000/orders/daily_metrics'),
+      );
 
-    const dailyMetricData = {
-      total_processed_olives: response.data.total_processed_olives,
-      total_oil_made: response.data.total_oil_made,
-      analyzed_at_time: response.data.analyzed_at_time,
-      avarage_oil_percentage: response.data.avarage_oil_percentage,
-    };
+      const dailyMetricData = {
+        total_processed_olives: response.data.total_processed_olives,
+        total_oil_made: response.data.total_oil_made,
+        analyzed_at_time: response.data.analyzed_at_time,
+        avarage_oil_percentage: response.data.avarage_oil_percentage,
+      };
 
-    const createdDailyMetric = await this.dailyMetric.create(dailyMetricData);
-
-    this.dailyMetric.insertMany(dailyMetricData);
-
-    return createdDailyMetric;
+      // Save the document to MongoDB
+      const createdDailyMetric = await this.dailyMetric.create(dailyMetricData);
+      console.log('Created Daily Metric:', createdDailyMetric);
+    } catch (error) {
+      // Handle errors appropriately
+      console.error('Error fetching and saving daily metrics:', error);
+    }
   }
 }
